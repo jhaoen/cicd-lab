3dab2ddb22794acf345a077dda98ab158eb3b016
jhaoen <jhaoen1106@gmail.com>
2026-05-19 11:28:42 +0800

    add bad format to make prettier fail

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import Fastify, { FastifyServerOptions } from 'fastify';
 
 export function buildApp(options: FastifyServerOptions = {}) {
+  const bad_format = 'this will fail prettier';
+
   const app = Fastify({
     logger: options.logger ?? true,
     ...options
